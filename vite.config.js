8f76ce48c7e6c7093ee3b783b60b61d5602a0343
DEV-TYY <suontii2023@gmail.com>
2025-12-08 10:21:40 -0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/tyy-dev/",  // correct for https://dev-tyy.github.io/tyy-dev/
+  base: "/tyy-dev/",   // 👈 MUST MATCH your GitHub repo name
 });
